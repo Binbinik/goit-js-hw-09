d79db904bf64bf62153b8b0d0642b08a625220bc
Binbinik <111363152+Binbinik@users.noreply.github.com>
2023-06-11 08:42:01 +0000

    Deploying to gh-pages from @ Binbinik/goit-js-hw-09@7b896a33b4449f43ab32c80562cada405e060d5b 🚀

---
+++ 01-color-switcher.8f7b699f.js
@@ -0,0 +1,2 @@
+!function(){var t=null;var e=document.querySelector("#start"),n=document.querySelector("#stop");e.addEventListener("click",(function(){e.disabled=!0,n.disabled=!1,t=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),n.addEventListener("click",(function(){n.disabled=!0,e.disabled=!1,clearInterval(t)}))}();
+//# sourceMappingURL=01-color-switcher.8f7b699f.js.map
